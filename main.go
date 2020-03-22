ab5a3fddb2f9e80977e185a5473f810c11b2f913
Ali Shalabi <ashalabi13@gmail.com>
2020-03-22 16:48:27 -0700

    Added method to draw card

--- main.go
+++ main.go
@@ -17,14 +17,26 @@ func(h Hand) String() string {
   return strings.Join(strgs, ", ")
 }
 
+func Draw(cards []deck.Card) (deck.Card, []deck.Card) {
+  return cards[0], cards[1:]
+}
+
 func main() {
-  // fmt.println("Hello world")
   cards := deck.New(deck.Deck(3), deck.Shuffle)
   var card deck.Card
-  for i := 0; i < 10; i++ {
-    card, cards = cards[0], cards[1:] // Draw card, remove from deck
-    fmt.Println(card)
+  // for i := 0; i < 10; i++ {
+  //   card, cards = cards[0], cards[1:] // Draw card, remove from deck
+  //   fmt.Println(card)
+  // }
+  // var h Hand = cards[0:3]
+  // fmt.Println(h)
+  var player, dealer Hand
+  for i := 0; i < 2 ; i++ { // Draw two cards
+    for _, hand := range []*Hand{&player, &dealer} {
+      card, cards = Draw(cards)
+      *hand = append(*hand, card)
+    }
   }
-  var h Hand = cards[0:3]
-  fmt.Println(h)
+  fmt.Println("Player's hand:", player)
+  fmt.Println("Dealer's hand:", dealer)
 }
