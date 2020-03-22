0c99a37814f676189205a72716c8f74a60c88662
Ali Shalabi <ashalabi13@gmail.com>
2020-03-21 17:59:02 -0700

    Taking coding break

--- main.go
+++ main.go
@@ -2,8 +2,20 @@ package main
 
 import (
   "github.com/alishalabi/deck"
+  "strings"
   "fmt"
 )
+
+type Hand []deck.Card
+
+// String returns all cards in hand, in print-friendly format
+func(h Hand) String() string {
+  strgs := make([]string, len(h))
+  for i, _ := range h {
+    strgs[i] = h[i].String()
+  }
+  return strings.Join(strgs, ", ")
+}
 
 func main() {
   // fmt.println("Hello world")
@@ -13,4 +25,6 @@ func main() {
     card, cards = cards[0], cards[1:] // Draw card, remove from deck
     fmt.Println(card)
   }
+  var h Hand = cards[0:3]
+  fmt.Println(h)
 }
