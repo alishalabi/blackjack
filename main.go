18d594838dec0a99045dd9d2567a710829eaf79a
Ali Shalabi <ashalabi13@gmail.com>
2020-03-22 17:08:54 -0700

    Finished basic functionality, can hit or stand

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 type Hand []deck.Card
 
 // String returns all cards in hand, in print-friendly format
-func(h Hand) String() string {
+func (h Hand) String() string {
   strgs := make([]string, len(h))
   for i, _ := range h {
     strgs[i] = h[i].String()
@@ -17,26 +17,45 @@ func(h Hand) String() string {
   return strings.Join(strgs, ", ")
 }
 
-func Draw(cards []deck.Card) (deck.Card, []deck.Card) {
+// DealerString hides the dealer's second hand
+func (h Hand) DealerString() string {
+  return h[0].String() + ", **Hidden**"
+}
+
+// Draw takes the top card of the deck, then returns that card and a diminshed deck
+func draw(cards []deck.Card) (deck.Card, []deck.Card) {
   return cards[0], cards[1:]
 }
 
 func main() {
   cards := deck.New(deck.Deck(3), deck.Shuffle)
   var card deck.Card
-  // for i := 0; i < 10; i++ {
-  //   card, cards = cards[0], cards[1:] // Draw card, remove from deck
-  //   fmt.Println(card)
-  // }
-  // var h Hand = cards[0:3]
-  // fmt.Println(h)
   var player, dealer Hand
   for i := 0; i < 2 ; i++ { // Draw two cards
-    for _, hand := range []*Hand{&player, &dealer} {
-      card, cards = Draw(cards)
+    for _, hand := range []*Hand{&player, &dealer} { // Using pointers to avoid "hand" copies
+      card, cards = draw(cards)
       *hand = append(*hand, card)
     }
   }
-  fmt.Println("Player's hand:", player)
-  fmt.Println("Dealer's hand:", dealer)
+
+  // Game run logic
+  var input string
+  for input != "s" {
+    fmt.Println("Player's hand:", player)
+    fmt.Println("Dealer's hand:", dealer.DealerString())
+    fmt.Println("Player's choice: (h)it or (s)tand")
+    fmt.Scanf("%s\n", &input)
+    switch input {
+    case "h":
+      card, cards = draw(cards)
+      player = append(player, card)
+    default:
+      fmt.Println("Whoops, not a valid option. Please enter *h* to hit, or *s* to stand.")
+    }
+  }
+  fmt.Println("***Final Hands***")
+  fmt.Println("Player's final hand:", player)
+  fmt.Println("Dealer's final hand:", dealer)
+
+
 }
